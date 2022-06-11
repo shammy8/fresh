2865dd7ead1c080558c8d5c95efe4640cc7f6db6
Allan Sham <allansham@hotmail.com>
2022-06-11 02:11:06 +0100

    ng add material, change css to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'fresh-root',
-  template: ` hi `,
+  template: ` <mat-toolbar color="primary"><h1>Fresh</h1></mat-toolbar>`,
   styles: [],
 })
 export class AppComponent {
