1b5b23ee6a6e59d992b02f4255ab8e2794927441
Allan Sham <allansham@hotmail.com>
2022-06-11 02:50:39 +0100

    ng add angular fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -13,7 +17,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'fresh-4c845',
+    appId: '1:963700420428:web:431be7be7405e53e20a9e9',
+    storageBucket: 'fresh-4c845.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDVAIfAlfnSw0pTnFbqW9efCEz2195ZuFw',
+    authDomain: 'fresh-4c845.firebaseapp.com',
+    messagingSenderId: '963700420428',
+    measurementId: 'G-Z77W3HZJGD',
+  },
 };
 
 /*
