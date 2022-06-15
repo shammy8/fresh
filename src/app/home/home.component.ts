2c282d8a1b896d3201ff18cea3832c9ab9f293dd
Allan Sham <allansham@hotmail.com>
2022-06-15 17:58:00 +0100

    get the items from firestore

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { map, Observable, of, switchMap } from 'rxjs';
+
+import {
+  collection,
+  collectionData,
+  Firestore,
+  limit,
+  query,
+} from '@angular/fire/firestore';
 
 @Component({
   selector: 'fresh-home',
-  template: ` <p>home works!</p> `,
+  template: `
+    <pre>{{ items$ | async | json }} </pre>
+  `,
   styles: [],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  items$: Observable<any> = of([]); // TODO
 
-  ngOnInit(): void {}
+  constructor(private _route: ActivatedRoute, private _firestore: Firestore) {}
+
+  ngOnInit(): void {
+    this.items$ = this._route.paramMap.pipe(
+      switchMap((params) => {
+        const homeId = params.get('homeId');
+        console.log(homeId);
+        const itemsQuery = query(
+          collection(this._firestore, `home/${homeId}/items`),
+          limit(10)
+        );
+        return collectionData(itemsQuery);
+      })
+    );
+  }
 }
