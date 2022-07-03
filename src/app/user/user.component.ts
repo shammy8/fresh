4262d918296b54f5eede02bc43f9edc6d54f6526
Allan Sham <allansham@hotmail.com>
2022-07-03 17:02:56 +0100

    move user template to separate file

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnDestroy } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -7,85 +7,14 @@ import {
   MAT_BOTTOM_SHEET_DATA,
 } from '@angular/material/bottom-sheet';
 
-import { Observable, Subject, takeUntil } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { UserService } from '../services/user.service';
 import { UserDetails } from '../item.interface';
 
 @Component({
   selector: 'fresh-user',
-  template: `
-    <div *ngrxLet="data.userDoc$ as userDoc" class="grid">
-      <ng-container *ngIf="!isEditMode; else editModeTemplate">
-        <h2>
-          {{ userDoc.displayName }}
-        </h2>
-
-        <button
-          mat-icon-button
-          matTooltip="Edit display name"
-          (click)="onEdit(userDoc.displayName)"
-        >
-          <mat-icon>edit</mat-icon>
-        </button>
-      </ng-container>
-
-      <ng-template #editModeTemplate>
-        <mat-form-field>
-          <input
-            matInput
-            [formControl]="newNameControl"
-            placeholder="New display name"
-            name="displayName"
-          />
-          <mat-error *ngIf="newNameControl.hasError('maxlength')"
-            >Name must be less than 31 characters</mat-error
-          >
-        </mat-form-field>
-
-        <div class="name-control-button-container">
-          <button
-            mat-icon-button
-            matTooltip="Cancel"
-            (click)="isEditMode = false"
-          >
-            <mat-icon>clear</mat-icon>
-          </button>
-          <button
-            mat-icon-button
-            matTooltip="Save display name"
-            (click)="saveDisplayName(userDoc.uid)"
-          >
-            <mat-icon>check</mat-icon>
-          </button>
-        </div>
-      </ng-template>
-
-      <p>{{ userDoc.uid }}</p>
-
-      <button
-        mat-icon-button
-        [cdkCopyToClipboard]="userDoc.uid"
-        matTooltip="Copy user ID"
-      >
-        <mat-icon>content_copy</mat-icon>
-      </button>
-
-      <p>{{ userDoc.email }}</p>
-      <button
-        mat-icon-button
-        [cdkCopyToClipboard]="userDoc.email"
-        matTooltip="Copy email"
-      >
-        <mat-icon>content_copy</mat-icon>
-      </button>
-
-      <div></div>
-      <button mat-button type="button" (click)="closeBottomSheet()">
-        Close
-      </button>
-    </div>
-  `,
+  templateUrl: './user.component.html',
   styles: [
     `
       .grid {
@@ -105,15 +34,13 @@ import { UserDetails } from '../item.interface';
     `,
   ],
 })
-export class UserComponent implements OnDestroy {
+export class UserComponent {
   newNameControl = new FormControl('', {
     nonNullable: true,
     validators: [Validators.maxLength(30)],
   });
 
   isEditMode = false;
-
-  private _destroy = new Subject<void>();
 
   constructor(
     private _snackBar: MatSnackBar,
@@ -145,10 +72,5 @@ export class UserComponent implements OnDestroy {
 
   closeBottomSheet() {
     this._bottomSheetRef.dismiss();
-  }
-
-  ngOnDestroy() {
-    this._destroy.next();
-    this._destroy.complete();
   }
 }
