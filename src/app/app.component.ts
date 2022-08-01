b51445e4d527abc5eb90e186a0906eaf7be65c25
Allan Sham <allansham@hotmail.com>
2022-08-01 17:05:22 +0100

    add loading spinner when app is loading first route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,34 @@
+import { NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    NgIf,
+    // IfModule, // TODO doesn't remove the spinner once route is loaded
+    MatProgressSpinnerModule,
+  ],
   selector: 'fresh-root',
-  template: ` <router-outlet></router-outlet> `,
+  template: `
+    <mat-spinner *ngIf="!isRouteLoaded"></mat-spinner>
+    <router-outlet (activate)="isRouteLoaded = !!$event"></router-outlet>
+  `,
+  styles: [
+    `
+      mat-spinner {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      }
+    `,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  isRouteLoaded = false;
+}
